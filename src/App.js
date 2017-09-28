7a39cdb5da688b65d0a627bfb1e0f7ccd92fff34
timsunpang <timsunpang@gmail.com>
2017-09-27 23:33:54 -0700

    finished todo tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Let's get started.
-        </p>
+      <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
       </div>
     );
   }
